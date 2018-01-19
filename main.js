17a5dd636bd4d3edaf27ec6e5dd3513309a95f93
finley <sssrrr@vip.qq.com>
2018-01-19 16:08:55 +0800

    commit

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-keys = {
+//1.初始化数据
+var keys = {
   0: ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
   1: ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
   2: ["z", "x", "c", "v", "b", "n", "m"],
   length: 3
 };
-hash = {
+var hash = {
   q: "qq.com",
   w: "weibo.com",
   e: "ele.me",
@@ -20,41 +21,73 @@ hash = {
   d: "douyu.com",
   z: "zhihu.com"
 };
-hashInLocalStorage = JSON.parse(localStorage.getItem("uuu") || "null"); //取出localStorage中uuu对应的hash
+
+function getFromLocalStorage(name) {
+  return JSON.parse(localStorage.getItem(name) || "null");
+}
+
+var hashInLocalStorage = getFromLocalStorage("uuu"); //取出localStorage中uuu对应的hash
 if (hashInLocalStorage) {
   hash = hashInLocalStorage;
 } //判断有localstorage就替换掉hash
-index = 0;
+
+//2.生成键盘
 //遍历keys,生成kbd标签
+var index = 0;
 while (index < keys.length) {
   // 0 1 2 循环创建div
-  div1 = document.createElement("div"); //创建div
+  var div1 = document.createElement("div"); //创建div
+  div1.className = "row";
   main1.appendChild(div1); //添加儿子
-  row = keys[index]; //第一个子数组，第二个子数组，第三个子数组
-  index2 = 0;
+  var row = keys[index]; //第一个子数组，第二个子数组，第三个子数组
+  var index2 = 0;
   while (index2 < row.length) {
     //添加多少个kbd取决与keys的子数组个数  index2= 0~9 0~8 0~6
-    kbd = document.createElement("kbd"); //创建kbd
-    kbd.textContent = row[index2]; //把key[0,1,2]中的字符串添加到kbd中
-    button1 = document.createElement("button"); //创建button
+    var kbd = document.createElement("kbd"); //创建kbd
+    var span = document.createElement("span");
+    span.textContent = row[index2]; //把key[0,1,2]中的字符串添加到kbd中
+    kbd.className = "key";
+    var button1 = document.createElement("button"); //创建button
     button1.textContent = "编辑"; //button内容
     button1.id = row[index2]; //给botton添加id
+    var img = document.createElement("img");
+    if (hash[row[index2]]) {
+      //如果设置存在把图标设置为src
+      img.src = "http://" + hash[row[index2]] + "/favicon.ico";
+    } else {
+      //不存在设置为透明
+      img.src = "//i.loli.net/2017/11/10/5a05afbc5e183.png";
+    }
+    img.onerror = function(xxx) {
+      xxx.target.src = "//i.loli.net/2017/11/10/5a05afbc5e183.png";
+    };
+
     button1.onclick = function(xxx) {
-      key = xxx.target.id; //拿到用户点击的取值
-      userUrl = prompt("请输入网址"); //用户输入的网址
+      var button2 = xxx.target; //拿到用户点击的取值
+      var key = button2.id;
+      var userUrl = prompt("请输入网址"); //用户输入的网址
       hash[key] = userUrl; //改变hash的对应默认网址
+      var img2 = button2.previousSibling;
+      img2.src = "http://" + userUrl + "/favicon.ico";
+      img2.onerror = function(xxx) {
+        xxx.target.src = "//i.loli.net/2017/11/10/5a05afbc5e183.png";
+      }; //图片为加载保险事件
       localStorage.setItem("uuu", JSON.stringify(hash));
     };
+    kbd.appendChild(span);
+    kbd.appendChild(img);
     kbd.appendChild(button1); //添加booton到kbd中
     div1.appendChild(kbd); //添加kbd到div中
     index2++;
   }
   index++;
 }
-//监听用户按键
+//3.监听用户按键
 document.onkeypress = function(xxxx) {
   //xxxx中包含你按键的所有信息
-  key = xxxx["key"]; //获用户的按键
-  wbsite = hash[key]; //绑定按键到对应网站
+  var key = xxxx["key"]; //获用户的按键
+  var wbsite = hash[key]; //绑定按键到对应网站
   window.open("http://" + wbsite, "_blank"); //在新窗口打开
 };
+
+
